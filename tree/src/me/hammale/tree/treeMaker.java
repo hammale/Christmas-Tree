60817661a2306172fbdf07e7eb099a8d801743f6
alex <alex@192.168.55.104>
2011-12-12 19:44:03 -0600

    almost done :D (yeah rite)

--- tree/src/me/hammale/tree/treeMaker.java
+++ tree/src/me/hammale/tree/treeMaker.java
@@ -143,6 +143,8 @@ public class treeMaker {
 					//end corner 2//
 					
 					//START OTHER SIDE
+					
+					set3 = set3.getRelative(BlockFace.EAST, 19);
 									
 					if(i1 == 7  && i == 1){
 
@@ -262,6 +264,135 @@ public class treeMaker {
 					}
 								
 					//end corner 2//
+					
+					
+					
+					
+					
+					set3 = set3.getRelative(BlockFace.WEST, 5);
+					
+					if(i1 == 7  && i == 1){
+
+						while(s < 7){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 6  && i == 2){
+
+						while(s < 8){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 5  && i == 3){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 4  && i == 4){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 3  && i == 5){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 2  && i == 6){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 1  && i == 7){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.NORTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}
+					
+					//start corner 2//
+					if(i1 == 20  && i == 7){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 19  && i == 6){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 18  && i == 5){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 17  && i == 4){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 16  && i == 3){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 15  && i == 2){
+
+						while(s < 9){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}else if(i1 == 14  && i == 1){
+
+						while(s < 7){
+							Block clr = set3.getRelative(BlockFace.SOUTH, s);
+							clr.setTypeId(18);
+							s++;
+						}
+						s = 1;
+					}
+								
+					//end corner 2//
+					
+					
+					
+					
 					
 					//END OTHER SIDE
 					
